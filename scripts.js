932c079fe0e60ad08a79e7ae156d34eb42188101
Allegra Megan Anka <allegra.anka@gmail.com>
2020-05-29 20:32:27 -0400

    fetching random photo from unsplash, injecting styling into DOM

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ let dateslot = document.getElementById("date");
 dateslot.innerHTML = moment().format("ddd, MMM Do");
 document.onload = window.setInterval(getTime, 1000);
 
+setWeather();
+
 function getTime() {
     let d = new Date();
     let t = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
@@ -53,30 +55,28 @@ async function getWeather() {
 }
 
 // set the city on the city field
-let cityslot = document.getElementById("city");
+var cityslot = document.getElementById("city");
 
 // set the temperature on the temperature field
-let temperatureField = document.getElementById("temp");
-let feelsLikeField = document.getElementById("feelsLike");
-let summaryField = document.getElementById("summary");
+var temperatureField = document.getElementById("temp");
+var feelsLikeField = document.getElementById("feelsLike");
+var summaryField = document.getElementById("summary");
 
 async function setWeather() {
-    var temperature = await getWeather().then(weatherDetails => weatherDetails.temp);
-    var feelsLike = await getWeather().then(weatherDetails => weatherDetails.feelsLike);
-    var summary = await getWeather().then(weatherDetails => weatherDetails.summary);
+    let temperature = await getWeather().then(weatherDetails => weatherDetails.temp);
+    let feelsLike = await getWeather().then(weatherDetails => weatherDetails.feelsLike);
+    let summary = await getWeather().then(weatherDetails => weatherDetails.summary);
     
     temperatureField.innerHTML += `Temperature: ${temperature}&degF`;
     feelsLikeField.innerHTML += `Feels like: ${feelsLike}&degF`;
     summaryField.innerHTML += `Outside Currently: ${summary}`;
 }
 
-setWeather();
-
 async function getPhoto() {
-    var query = await getWeather().then(weatherDetails => weatherDetails.summary);
-    var client_id = 'vcy9-edsyORibDbHn8niyPoKb5N2JXatDpvs_A0XYME';
-    var baseUrl = 'https://api.unsplash.com';
-    var endpoint = '/photos/random';
+    let query = await getWeather().then(weatherDetails => weatherDetails.summary);
+    let client_id = 'vcy9-edsyORibDbHn8niyPoKb5N2JXatDpvs_A0XYME';
+    let baseUrl = 'https://api.unsplash.com';
+    let endpoint = '/photos/random';
 
     return fetch(`${baseUrl}${endpoint}?client_id=${client_id}&query=${query}`)
     .then(response => response.json())
@@ -97,4 +97,31 @@ async function getPhoto() {
     .catch(error => console.log(`There was an error fetching photo: ${error}`));
 }
 
-getPhoto().then(photoDetails => console.log("photo taken with: ", photoDetails.photo.takenWith));
+// getPhoto().then(photoDetails => console.log("photo taken with: ", photoDetails.photo.takenWith));
+
+// set background photo with random photo from unsplash
+var photographerNameField = document.getElementById("photographer_name");
+var photographerFromField = document.getElementById("photographer_from");
+var photographerBioField = document.getElementById("photographer_bio");
+var photoTakenWithField = document.getElementById("photo_taken_with");
+
+async function setPhoto() {
+    let photographerName = await getPhoto().then(photoDetails => photoDetails.photographer.name);
+    let photographerLoc = await getPhoto().then(photoDetails => photoDetails.photographer.from);
+    let photographerBio = await getPhoto().then(photoDetails => photoDetails.photographer.bio);
+    let photoUrl = await getPhoto().then(photoDetails => photoDetails.photo.photoUrl);
+    let photoTakenWith = await getPhoto().then(photoDetails => photoDetails.photo.takenWith);
+
+    photographerNameField.innerHTML += photographerName;
+    photographerFromField.innerHTML += photographerLoc;
+    photographerBioField.innerHTML += photographerBio;
+    photoTakenWithField.innerHTML += photoTakenWith;
+
+    let styling = `body { background: #1a1e24 no-repeat center url(${photoUrl}); background-blend-mode: overlay; color: white; }`;
+    let style = document.createElement('style');
+    style.appendChild(document.createTextNode(styling));
+
+    document.head.appendChild(style);
+}
+
+setPhoto();
